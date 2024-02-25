71d9f036cdaae51ddae92c269048ba2eda7c1d5d
ocin14 <new.nikolee2007@gmail.com>
2024-02-25 21:57:01 +0700

    commit

--- src/components/register.vue
+++ src/components/register.vue
@@ -2,16 +2,38 @@
   <img class="logo" src="../assets/logoff.png">
   <h2 style="color:#a4652d">Sign Up</h2>
   <div class="register">
-  <input type="text" placeholder="Name">
-  <input type="text" placeholder="Email">
-  <input type="text" placeholder="Password">
-  <button>Sign Up</button>
+  <input type="text" v-model="name" placeholder="Name">
+  <input type="text" v-model="email" placeholder="Email">
+  <input type="text" v-model="password" placeholder="Password">
+  <button v-on:click="signup">Sign Up</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name:'RegisterForm'
+    name:'RegisterForm',
+    data(){
+      return{
+        name:'',
+        email:'',
+        password:''
+      }
+    },
+    methods:{
+      async signup(){
+        let result = await axios.post("http://localhost:3000/user",{
+       email:this.email,
+       password:this.password,
+       name:this.name
+        });
+        console.warn(result);
+        if(result.status==201){
+          alert("berhasil");
+          localStorage.setItem("user-info",JSON.stringify(result.data))
+        }
+      }
+    }
 }
 </script>
 
