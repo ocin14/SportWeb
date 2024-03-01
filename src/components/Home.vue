2f76b46ea9035b5a619a49ca0e45407e5d103c84
ocin14 <new.nikolee2007@gmail.com>
2024-03-01 18:56:48 +0700

    homepage commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,23 @@
 <template>
-  <h1>Hello, Welcome on fitfun website</h1>
-  <h2>enjoy your life</h2>
+  <div>
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h3><strong>Welcome on</strong><br> FitFun Website</h3>
+              <p>"Enjoy your life"</p>
+              <router-link class="btn btn-lg btn-success" to="/Foods" style="background-color: #B80000;">Pesan <b-icon-arrow-right></b-icon-arrow-right></router-link>
+            </div>
+          </div>
+        </div>
+        <div class="img">
+          <img src="../assets/Fitness stats-bro.svg"  >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,5 +33,8 @@ export default {
 </script>
 
 <style>
-
+  img{
+    height: 600px;
+    padding-left: 130px;
+  }
 </style>
